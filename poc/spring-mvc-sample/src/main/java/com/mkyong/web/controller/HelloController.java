0b98585237103a4470ff66e8e9d695156e553942
gopinath <gopimkd.dd@gmail.com>
2018-07-28 17:00:44 +0530

    dfgdg

--- poc/spring-mvc-sample/src/main/java/com/mkyong/web/controller/HelloController.java
+++ poc/spring-mvc-sample/src/main/java/com/mkyong/web/controller/HelloController.java
@@ -13,8 +13,7 @@ public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
-		model.addAttribute("message", "Spring 3 MVC Hello World check");
-		sysout
+		model.addAttribute("message", "Spring 3 MVC Hello World");
 		return "hello";
 
 	}
